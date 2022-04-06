cb398120134e7ed20a6e90996f6cf63b0de5c762
AkaZeLL <102630630+aKaZeLL@users.noreply.github.com>
2022-04-06 19:14:43 +0200

    aggiunte funzioni check punto e struct deck

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+	"sort"
 )
 
 type Card struct {
@@ -11,25 +12,21 @@ type Card struct {
 	Seed   string
 }
 
+type Deck struct {
+	Cards []*Card
+}
+
+//Costruttore per struttura Card
 func New(v int, s string) *Card {
 	return &Card{Value: v, Seed: s}
 }
 
-func (c Card) String() string {
-
-	if c.Value > 1 && c.Value < 11 {
-		return fmt.Sprintf("%3d di %5s", c.Value, c.Seed)
-	}
-
-	seeds := map[int]string{1: "A", 11: "J", 12: "Q", 13: "K"}
-	return fmt.Sprintf("%2s di %5s", seeds[c.Value], c.Seed)
-}
-
-func CreateDeck() []*Card {
+//Costruttore struttura Deck
+func CreateDeck(start int) *Deck {
 	seeds := []string{"Cuori", "Quadri", "Picche", "Fiori"}
 	var deck []*Card
 	for _, c := range seeds {
-		for num := 1; num <= 13; num++ {
+		for num := start; num <= 14; num++ {
 			deck = append(deck, New(num, c))
 		}
 	}
@@ -38,29 +35,105 @@ func CreateDeck() []*Card {
 	rand.Shuffle(len(deck), func(i, j int) {
 		deck[i], deck[j] = deck[j], deck[i]
 	})
-	return deck
+	return &Deck{Cards: deck}
 }
 
-func DrawHand(deck []*Card) ([]*Card, []*Card) {
-	temp := deck[:5]
-	deck = deck[5:]
-	return temp , deck
+func (c Card) String() string {
+	if c.Value > 1 && c.Value < 11 {
+		return fmt.Sprintf("%3d di %5s", c.Value, c.Seed)
+	}
+	seeds := map[int]string{11: "J", 12: "Q", 13: "K", 14: "A"}
+	return fmt.Sprintf("%2s di %5s", seeds[c.Value], c.Seed)
+}
+
+func (d *Deck) DrawHand(n int) []*Card {
+	temp := d.Cards[:n]
+	d.Cards = d.Cards[n:]
+	return temp
+}
+
+func Flush(hand []*Card) bool {
+	color := hand[0].Seed
+	for _, m := range hand {
+		if m.Seed != color {
+			return false
+		}
+	}
+	return true
+}
+
+func StructToArray(hand []*Card) []int {
+	var array []int
+	for _, h := range hand {
+		array = append(array, h.Value)
+	}
+	return array
+}
+
+func Straight(hand []*Card) bool {
+	array := StructToArray(hand)
+	sort.Ints(array)
+	
+	for i:=0; i<len(hand)-1; i++ {
+		if array[i]!=array[i+1]-1 {
+			return false
+		}
+	}
+	return true
+}
+
+func Royale(hand []*Card) bool {
+	if Straight(hand) && Flush(hand) {
+		return true
+	} else {
+		return false
+	}
+}
+
+func count(hand []*Card, q int) int {
+	array := StructToArray(hand)
+
+	for i:=0; i<len(array)-1; i++ {
+		cnt := 1
+		for x:=i+1; x<len(array); i++ {
+			if array[i]==array[x] {
+				cnt+=1
+			}
+		}
+		if cnt==q {
+			return array[i]
+			}
+	}
+	return 0
 }
 
 func Print(hand []*Card) {
 	for _, h:= range hand {
 		fmt.Print(h.String())
 	}
+	fmt.Println()
 }
+
+/*
+reale OK
+poker
+colore OK
+full
+scala semplice OK
+tris
+doppia coppia
+coppia
+carta alta
+*/
 
 func main() {
 
-	var deck = CreateDeck()
+	var deck = CreateDeck(7)
+//	for i:=1; i<5; i++ {
+	hand := deck.DrawHand(5)
 
-	hand, deck := DrawHand(deck)
-	
 	Print(hand)
-//	for _, value := range deck {
-//		fmt.Println(value.String())
-//	}
+
+
+//}
 }
