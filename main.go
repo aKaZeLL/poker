6937f62369f1a842347ab52787b600601de97135
AkaZeLL <102630630+aKaZeLL@users.noreply.github.com>
2022-04-06 13:56:20 +0200

    cambiati i nomi in inglese

--- main.go
+++ main.go
@@ -6,49 +6,49 @@ import (
 	"time"
 )
 
-type Carta struct {
-	Valore int
-	Seme   string
+type Card struct {
+	Value int
+	Seed   string
 }
 
-func New(v int, s string) *Carta {
-	return &Carta{Valore: v, Seme: s}
+func New(v int, s string) *Card {
+	return &Card{Value: v, Seed: s}
 }
 
-func (c Carta) String() string {
-	if c.Valore == 1 {
-		return fmt.Sprintf("A %s", c.Seme)
+func (c Card) String() string {
+	if c.Value == 1 {
+		return fmt.Sprintf("A %s", c.Seed)
 	}
 
-	if c.Valore < 11 {
-		return fmt.Sprintf("%d %s", c.Valore, c.Seme)
+	if c.Value < 11 {
+		return fmt.Sprintf("%d %s", c.Value, c.Seed)
 	}
 
 	semi := map[int]string{1: "A", 11: "J", 12: "Q", 13: "K"}
-	return fmt.Sprintf("%s %s", semi[c.Valore], c.Seme)
+	return fmt.Sprintf("%s %s", semi[c.Value], c.Seed)
 }
 
-func CreaMazzo() [52]*Carta {
-	colori := []string{"Cuori", "Quadri", "Picche", "Fiori"}
-	var mazzo [52]*Carta
+func CreateDeck() [52]*Card {
+	seeds := []string{"Cuori", "Quadri", "Picche", "Fiori"}
+	var deck [52]*Card
 	i := 0
-	for _, c := range colori {
+	for _, c := range seeds {
 		for num := 1; num <= 13; num++ {
-			mazzo[i] = New(num, c)
+			deck[i] = New(num, c)
 			i++
 		}
 	}
 	//Mescola
 	rand.Seed(time.Now().UnixNano())
-	rand.Shuffle(len(mazzo), func(i, j int) {
-		mazzo[i], mazzo[j] = mazzo[j], mazzo[i]
+	rand.Shuffle(len(deck), func(i, j int) {
+		deck[i], deck[j] = deck[j], deck[i]
 	})
-	return mazzo
+	return deck
 }
 
 func main() {
-	var carte = CreaMazzo()
-	for _, k := range carte {
-		fmt.Println(k.String())
+	var deck = CreateDeck()
+	for _, value := range deck {
+		fmt.Println(value.String())
 	}
 }
