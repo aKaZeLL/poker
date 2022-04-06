bdd78b57d6035d0eb21d39935d4c74ff815c83da
Fabrizio Gargiulo <fabrizio.gargiulo@sourceability.com>
2022-04-06 12:14:27 +0200

    Una proposta

--- main.go
+++ main.go
@@ -11,22 +11,17 @@ type Carta struct {
 	Seme   string
 }
 
-func (c Carta) IsFigure() bool {
-	switch c.Valore {
-	case 1, 11, 12, 13:
-		return true
-	default:
-		return false
-	}
-}
-
-func (c Carta) ToFig() string {
-	semi := map[int]string{1: "A", 11: "J", 12: "Q", 13: "K"}
-	return semi[c.Valore]
-}
-
 func New(v int, s string) *Carta {
 	return &Carta{Valore: v, Seme: s}
+}
+
+func (c Carta) String() string {
+	if c.Valore < 11 {
+		return fmt.Sprintf("%s", c.Valore)
+	}
+
+	semi := map[int]string{1: "A", 11: "J", 12: "Q", 13: "K"}
+	return semi[c.Valore]
 }
 
 func CreaMazzo() [52]*Carta {
@@ -48,13 +43,8 @@ func CreaMazzo() [52]*Carta {
 }
 
 func main() {
-
-	var carte [52]*Carta = CreaMazzo()
+	var carte = CreaMazzo()
 	for _, k := range carte {
-		if k.IsFigure() {
-			fmt.Println(k.ToFig(), k.Seme)
-		} else {
-			fmt.Println(k.Valore, k.Seme)
-		}
+		fmt.Println(k.String())
 	}
 }
